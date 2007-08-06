58dc16406976d8c18191470a6bafaeda793ed523
mitchell <70453897+667e-11@users.noreply.github.com>
2007-08-06 05:04:35 -0400

    Initial import of the textadept module.

---
+++ modules/textadept/init.lua
@@ -0,0 +1,12 @@
+-- Copyright 2007 Mitchell mitchell<att>caladbolg.net. See LICENSE.
+
+---
+-- The textadept module.
+-- It provides utilities for editing text in Textadept.
+module('modules.textadept', package.seeall)
+
+require 'textadept.editing'
+require 'textadept.keys'
+require 'textadept.mlines'
+require 'textadept.snippets'
+require 'textadept.key_commands' -- last
