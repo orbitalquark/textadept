2c36e7e3675160ed03cf8d66d0d778229996b65d
mitchell <70453897+orbitalquark@users.noreply.github.com>
2007-08-06 05:04:51 -0400

    Initial import of the lua module.

---
+++ modules/lua/init.lua
@@ -0,0 +1,31 @@
+-- Copyright 2007 Mitchell mitchell<att>caladbolg.net. See LICENSE.
+
+---
+-- The lua module.
+-- It provides utilities for editing Lua code.
+module('modules.lua', package.seeall)
+
+if type(_G.snippets) == 'table' then
+---
+-- Container for Lua-specific snippets.
+-- @class table
+-- @name snippets.lua
+  _G.snippets.lua = {}
+end
+
+if type(_G.keys) == 'table' then
+---
+-- Container for Lua-specific key commands.
+-- @class table
+-- @name keys.lua
+  _G.keys.lua = {}
+end
+
+require 'lua.commands'
+require 'lua.snippets'
+
+function set_buffer_properties()
+
+end
+
+api = textadept.io.read_api_file(_HOME..'/modules/lua/api', '%w_.:')
