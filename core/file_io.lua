e7256abb8c7caeff8d274542b754799ebd3c8831
mitchell <70453897+orbitalquark@users.noreply.github.com>
2007-08-06 04:59:15 -0400

    Initial import of core Lua files.

---
+++ core/file_io.lua
@@ -0,0 +1,274 @@
+-- Copyright 2007 Mitchell mitchell<att>caladbolg.net. See LICENSE.
+
+---
+-- Provides file input/output routines for Textadept.
+-- Opens and saves files and sessions and reads API files.
+module('textadept.io', package.seeall)
+
+local handlers = textadept.handlers
+
+---
+-- [Local] Opens a file or goes to its already open buffer.
+-- @param filename The absolute path to the file to open.
+local function open_helper(filename)
+  if not filename then return end
+  for index, buffer in ipairs(textadept.buffers) do
+    if filename == buffer.filename then view:goto_buffer(index) return end
+  end
+  local buffer = textadept.new_buffer()
+  local f, err = io.open(filename)
+  if f then
+    buffer:set_text( f:read('*all') )
+    buffer:empty_undo_buffer()
+    f:close()
+  end
+  buffer.filename = filename
+  buffer:set_save_point()
+  handlers.handle('file_opened', filename)
+end
+
+---
+-- Opens a list of files.
+-- @param filenames A '|' separated list of filenames to open. If none
+--   specified, the user is prompted to open files from a dialog.
+-- @usage textadept.io.open(filename)
+function open(filenames)
+  if not filenames then
+    local directory = '--filename="'..(buffer.filename or '')..'"'
+    filenames = io.popen('zenity --file-selection --multiple '..
+      directory):read('*all')
+  end
+  for filename in filenames:gmatch('[^|\n]+') do open_helper(filename) end
+end
+
+---
+-- Saves the current buffer to a file.
+-- @param buffer The buffer to save. Its 'filename' property is used as the
+--   path of the file to save to. This must be the currently focused buffer.
+-- @usage buffer:save()
+function save(buffer)
+  textadept.check_focused_buffer(buffer)
+  if not buffer.filename then return save_as(buffer) end
+  local f, err = io.open(buffer.filename, 'w')
+  if f then
+    prepare = modules.textadept.editing.prepare_for_save
+    if prepare then prepare() end
+    local txt, _ = buffer:get_text(buffer.length)
+    f:write(txt)
+    f:close()
+    buffer:set_save_point()
+  else
+    handlers.error(err)
+  end
+end
+
+---
+-- Saves the current buffer to a file different than its filename property.
+-- @param buffer The buffer to save. This must be the currently focused buffer.
+-- @filename The new filepath to save the buffer to.
+-- @usage buffer:save_as(filename)
+function save_as(buffer, filename)
+  textadept.check_focused_buffer(buffer)
+  if not filename then
+    local directory = '--filename="'..(buffer.filename or '')..'"'
+    filename = io.popen('zenity --file-selection --save '..
+      directory..' --confirm-overwrite'):read('*all')
+  end
+  if #filename > 0 then
+    buffer.filename = filename:sub(1, -2) -- chomp
+    buffer:save()
+    handlers.handle('file_saved_as', filename)
+  end
+end
+
+---
+-- Saves all dirty buffers to their respective files.
+-- @usage textadept.io.save_all()
+function save_all()
+  local current_buffer = buffer
+  local current_index
+  for idx, buffer in ipairs(textadept.buffers) do
+    view:goto_buffer(idx)
+    if buffer == current_buffer then current_index = idx end
+    if buffer.filename and buffer.dirty then buffer:save() end
+  end
+  view:goto_buffer(current_index)
+end
+
+---
+-- Closes the current buffer.
+-- If the buffer is dirty, the user is prompted to continue. The buffer is not
+-- saved automatically. It must be done manually.
+-- @param buffer The buffer to close. This must be the currently focused
+--   buffer.
+-- @usage buffer:close()
+function close(buffer)
+  textadept.check_focused_buffer(buffer)
+  if buffer.dirty and os.execute('zenity --question --title Alert '..
+    '--text "Close without saving?"') ~= 0 then
+    return false
+  end
+  buffer:delete()
+  return true
+end
+
+---
+-- Closes all open buffers.
+-- If any buffer is dirty, the user is prompted to continue. No buffers are
+-- saved automatically. They must be saved manually.
+-- @usage textadept.io.close_all()
+function close_all()
+  while #textadept.buffers > 1 do
+    view:goto_buffer(#textadept.buffers)
+    if not buffer:close() then return end
+  end
+  buffer:close() -- the last one
+end
+
+---
+-- Loads a Textadept session file.
+-- Textadept restores split views, opened buffers, cursor information, and
+-- project manager details.
+-- @param filename The absolute path to the session file to load. Defaults to
+--   $HOME/.ta_session if not specified.
+-- @usage textadept.io.load_session(filename)
+function load_session(filename)
+  local textadept = textadept
+  local f = io.open(filename or os.getenv('HOME')..'/.ta_session')
+  local current_view, splits = 1, { [0] = {} }
+  if f then
+    for line in f:lines() do
+      if line:match('^buffer:') then
+        local anchor, current_pos, first_visible_line, filename =
+          line:match('^buffer: (%d+) (%d+) (%d+) (.+)$')
+        textadept.io.open(filename or '')
+        -- Restore saved buffer selection and view.
+        local anchor = tonumber(anchor) or 0
+        local current_pos = tonumber(current_pos) or 0
+        local first_visible_line = tonumber(first_visible_line) or 0
+        local buffer = buffer
+        buffer._anchor, buffer._current_pos = anchor, current_pos
+        buffer._first_visible_line = first_visible_line
+        buffer:line_scroll( 0,
+          buffer:visible_from_doc_line(first_visible_line) )
+        buffer:set_sel(anchor, current_pos)
+      elseif line:match('^%s*split%d:') then
+        local level, num, type, size =
+          line:match('^(%s*)split(%d): (%S+) (%d+)')
+        local view = splits[#level] and splits[#level][ tonumber(num) ] or view
+        splits[#level + 1] = { view:split(type == 'true') }
+        splits[#level + 1][1].size = tonumber(size) -- could be 1 or 2
+      elseif line:match('^%s*view%d:') then
+        local level, num, buf_idx = line:match('^(%s*)view(%d): (%d+)$')
+        local view = splits[#level][ tonumber(num) ] or view
+        view:goto_buffer( tonumber(buf_idx) )
+      elseif line:match('^current_view:') then
+        local view_idx, buf_idx = line:match('^current_view: (%d+)')
+        current_view = tonumber(view_idx) or 1
+      elseif line:match('^pm:') then
+        local width, text = line:match('^pm: (%d+) (.+)$')
+        textadept.pm.width = width or 0
+        textadept.pm.entry_text = text or ''
+        textadept.pm.activate()
+      end
+    end
+    f:close()
+    textadept.views[current_view]:focus()
+  end
+end
+
+---
+-- Saves a Textadept session to a file.
+-- Saves split views, opened buffers, cursor information, and project manager
+-- details.
+-- @param filename The absolute path to the session file to save. Defaults to
+--   $HOME/.ta_session if not specified.
+-- @usage textadept.io.save_session(filename)
+function save_session(filename)
+  local session = ''
+  local buffer_line = "buffer: %d %d %d %s\n" -- anchor, cursor, line, filename
+  local split_line = "%ssplit%d: %s %d\n" -- level, number, type, size
+  local view_line = "%sview%d: %d\n" -- level, number, doc index
+  -- Write out opened buffers. (buffer: filename)
+  local buffer_indices, offset = {}, 0
+  for idx, buffer in ipairs(textadept.buffers) do
+    if buffer.filename then
+      local current = buffer.doc_pointer == textadept.focused_doc_pointer
+      local anchor = current and 'anchor' or '_anchor'
+      local current_pos = current and 'current_pos' or '_current_pos'
+      local first_visible_line = current and 'first_visible_line' or
+        '_first_visible_line'
+      session = session..
+        buffer_line:format(buffer[anchor] or 0, buffer[current_pos] or 0,
+        buffer[first_visible_line] or 0, buffer.filename)
+      buffer_indices[buffer.doc_pointer] = idx - offset
+    else
+      offset = offset + 1 -- don't save untitled files in session
+    end
+  end
+  -- Write out split views.
+  local function write_split(split, level, number)
+    local c1, c2 = split[1], split[2]
+    local vertical, size = tostring(split.vertical), split.size
+    local spaces = (' '):rep(level)
+    session = session..split_line:format(spaces, number, vertical, size)
+    spaces = (' '):rep(level + 1)
+    if type(c1) == 'table' then
+      write_split(c1, level + 1, 1)
+    else
+      session = session..view_line:format(spaces, 1, c1)
+    end
+    if type(c2) == 'table' then
+      write_split(c2, level + 1, 2)
+    else
+      session = session..view_line:format(spaces, 2, c2)
+    end
+  end
+  local splits = textadept.get_split_table()
+  if type(splits) == 'table' then
+    write_split(splits, 0, 0)
+  else
+    session = session..view_line:format('', 1, splits)
+  end
+  -- Write out the current focused view.
+  local current_view = view
+  for idx, view in ipairs(textadept.views) do
+    if view == current_view then current_view = idx break end
+  end
+  session = session..("current_view: %d\n"):format(current_view)
+  -- Write out other things.
+  local pm = textadept.pm
+  session = session..("pm: %d %s\n"):format(pm.width, pm.entry_text)
+  -- Write the session.
+  local f = io.open(filename or os.getenv('HOME')..'/.ta_session', 'w')
+  if f then f:write(session) f:close() end
+end
+
+---
+-- Reads an API file.
+-- Each non-empty line in the API file is structured as follows:
+--   identifier (parameters) description
+-- Whitespace is optional, but can used for formatting. In description, '\\n'
+-- will be interpreted as a newline (\n) character. 'Overloaded' identifiers
+-- are handled appropriately.
+-- @param filename The absolute path to the API file to read.
+-- @param word_chars Characters considered to be word characters for
+--   determining the identifier to lookup. Its contents should be in Lua
+--   pattern format suitable for the character class construct.
+-- @usage textadept.io.read_api_file(filename, '%w_')
+function read_api_file(filename, word_chars)
+  local api = {}
+  local f = io.open(filename)
+  if f then
+    for line in f:lines() do
+      local func, params, desc =
+        line:match('(['..word_chars..']+)%s*(%b())(.*)$')
+      if func and params and desc then
+        if not api[func] then api[func] = {} end
+        api[func][ #api[func] + 1 ] = { params, desc }
+      end
+    end
+    f:close()
+  end
+  return api
+end
---
+++ core/handlers.lua
@@ -0,0 +1,355 @@
+-- Copyright 2007 Mitchell mitchell<att>caladbolg.net. See LICENSE.
+
+--- Handler module that handles Scintilla and Textadept notifications/events.
+module('textadept.handlers', package.seeall)
+
+local handlers = textadept.handlers
+
+---
+-- Adds a function to a handler.
+-- Every handler has a table of functions associated with it that are run when
+-- the handler is called by Textadept.
+-- @param handler The string handler name.
+-- @param f The Lua function to add.
+-- @param index Optional index to insert the handler into.
+function add_function_to_handler(handler, f, index)
+  local plural = handler..'s'
+  if not handlers[plural] then handlers[plural] = {} end
+  local funcs = handlers[plural]
+  if index then
+    table.insert(funcs, index, f)
+  else
+    funcs[#funcs+ 1] = f
+  end
+end
+
+---
+-- Calls every function added to a handler in sequence.
+-- If true or false is returned by any function, the iteration ceases.
+-- @param handler The string handler name.
+-- @param ... Arguments to the handler.
+function handle(handler, ...)
+  local plural = handler..'s'
+  if not handlers[plural] then return end
+  local funcs = handlers[plural]
+  for _, f in ipairs(funcs) do
+    local result = f( unpack{...} )
+    if result == true or result == false then return result end
+  end
+end
+
+---
+-- Reloads handlers.
+-- Clears each table of handlers for each handler function and reloads this
+-- module to reset to the default handlers.
+function reload()
+  package.loaded['handlers'] = nil
+  for handler in pairs(handlers) do
+    if handlers[handler..'s'] then handlers[handler..'s'] = nil end
+  end
+  require 'handlers'
+end
+
+-- Signals.
+function buffer_new()
+  return handle('buffer_new')
+end
+function buffer_deleted()
+  return handle('buffer_deleted')
+end
+function buffer_switch()
+  return handle('buffer_switch')
+end
+function view_new()
+  return handle('view_new')
+end
+function view_switch()
+  return handle('view_switch')
+end
+function quit()
+  return handle('quit')
+end
+function keypress(code, shift, control, alt)
+  return handle('keypress', code, shift, control, alt)
+end
+
+-- Scintilla notifications.
+function char_added(n)
+  return handle( 'char_added', string.char(n.ch) )
+end
+function save_point_reached()
+  return handle('save_point_reached')
+end
+function save_point_left()
+  return handle('save_point_left')
+end
+function double_click(n)
+  return handle('double_click', n.position, n.line)
+end
+function update_ui()
+  return handle('update_ui')
+end
+function macro_record(n)
+  return handle('macro_record', n.message, n.wParam, n.lParam)
+end
+function margin_click(n)
+  return handle('margin_click', n.margin, n.modifiers, n.position)
+end
+function user_list_selection(n)
+  return handle('user_list_selection', n.wParam, n.text)
+end
+function uri_dropped(n)
+  return handle('uri_dropped', n.text)
+end
+function call_tip_click(n)
+  return handle('call_tip_click', n.position)
+end
+function auto_c_selection(n)
+  return handle('auto_c_selection', n.lParam, n.text)
+end
+
+--- Map of Scintilla notifications to their handlers.
+local c = textadept.constants
+local scnnotifications = {
+  [c.SCN_CHARADDED] = char_added,
+  [c.SCN_SAVEPOINTREACHED] = save_point_reached,
+  [c.SCN_SAVEPOINTLEFT] = save_point_left,
+  [c.SCN_DOUBLECLICK] = double_click,
+  [c.SCN_UPDATEUI] = update_ui,
+  [c.SCN_MACRORECORD] = macro_record,
+  [c.SCN_MARGINCLICK] = margin_click,
+  [c.SCN_USERLISTSELECTION] = user_list_selection,
+  [c.SCN_URIDROPPED] = uri_dropped,
+  [c.SCN_CALLTIPCLICK] = call_tip_click,
+  [c.SCN_AUTOCSELECTION] = auto_c_selection
+}
+
+---
+-- Handles Scintilla notifications.
+-- @param n The Scintilla notification structure as a Lua table.
+function notification(n)
+  local f = scnnotifications[n.code]
+  if f then f(n) end
+end
+
+-- Default handlers to follow.
+
+add_function_to_handler('char_added',
+  function(char) -- auto-indent on return
+    if char ~= '\n' then return end
+    local buffer = buffer
+    local pos = buffer.current_pos
+    local curr_line = buffer:line_from_position(pos)
+    local last_line = curr_line - 1
+    while last_line >= 0 and #buffer:get_line(last_line) == 1 do
+      last_line = last_line - 1
+    end
+    if last_line >= 0 then
+      local indentation = buffer.line_indentation[last_line]
+      local s = buffer.line_indent_position[curr_line]
+      buffer.line_indentation[curr_line] = indentation
+      local e = buffer.line_indent_position[curr_line]
+      buffer:goto_pos(pos + e - s)
+    end
+  end)
+
+---
+-- [Local] Sets the title of the Textadept window to the buffer's filename.
+-- @param buffer The currently focused buffer.
+local function set_title(buffer)
+  local buffer = buffer
+  local filename = buffer.filename or 'Untitled'
+  local d = buffer.dirty and ' * ' or ' - '
+  textadept.title = filename:match('[^/]+$')..d..'Textadept'
+end
+
+add_function_to_handler('save_point_reached',
+  function() -- changes Textadept title to show 'clean' buffer
+    buffer.dirty = false
+    set_title(buffer)
+  end)
+
+add_function_to_handler('save_point_left',
+  function() -- changes Textadept title to show 'dirty' buffer
+    buffer.dirty = true
+    set_title(buffer)
+  end)
+
+---
+-- [Local table] A table of (integer) brace characters with their matches.
+-- @class table
+-- @name _braces
+local _braces = { -- () [] {} <>
+  [40] = 1, [91] = 1, [123] = 1, [60] = 1,
+  [41] = 1, [93] = 1, [125] = 1, [62] = 1,
+}
+
+---
+-- [Local] Highlights matching/mismatched braces appropriately.
+-- @param current_pos The position to match braces at.
+local function match_brace(current_pos)
+  local buffer = buffer
+  if _braces[ buffer.char_at[current_pos] ] and
+    buffer:get_style_name( buffer.style_at[current_pos] ) == 'operator' then
+    local pos = buffer:brace_match(current_pos)
+    if pos ~= -1 then
+      buffer:brace_highlight(current_pos, pos)
+    else
+      buffer:brace_bad_light(current_pos)
+    end
+    return true
+  end
+  return false
+end
+
+add_function_to_handler('update_ui',
+  function() -- highlights matching braces
+    local buffer = buffer
+    if not match_brace(buffer.current_pos) then buffer:brace_bad_light(-1) end
+  end)
+
+local docstatusbar_text = "Line: %d/%d Col: %d | Lexer: %s | %s | %s | %s"
+add_function_to_handler('update_ui',
+  function() -- sets docstatusbar text
+    local buffer = buffer
+    local pos = buffer.current_pos
+    local line, max = buffer:line_from_position(pos) + 1, buffer.line_count
+    local col = buffer.column[pos] + 1
+    local lexer = buffer:get_lexer_language()
+    local mode = buffer.overtype and 'OVR' or 'INS'
+    local eol = ( { 'CRLF', 'CR', 'LF' } )[buffer.eol_mode + 1]
+    local tabs = (buffer.use_tabs and 'Tabs:' or 'Spaces:')..buffer.indent
+    textadept.docstatusbar_text =
+      docstatusbar_text:format(line, max, col, lexer, mode, eol, tabs)
+  end)
+
+add_function_to_handler('margin_click',
+  function(margin, modifiers, position) -- toggles folding
+    local buffer = buffer
+    local line = buffer:line_from_position(position)
+    buffer:toggle_fold(line)
+  end)
+
+add_function_to_handler('buffer_new',
+  function() -- set additional buffer functions
+    local buffer, textadept = buffer, textadept
+    buffer.save = textadept.io.save
+    buffer.save_as = textadept.io.save_as
+    buffer.close = textadept.io.close
+    set_title(buffer)
+  end)
+
+add_function_to_handler('buffer_switch',
+  function() -- updates titlebar and statusbar
+    set_title(buffer)
+    update_ui()
+  end)
+
+add_function_to_handler('view_switch',
+  function() -- updates titlebar and statusbar
+    set_title(buffer)
+    update_ui()
+  end)
+
+add_function_to_handler('quit',
+  function() -- prompts for confirmation if any buffers are dirty; saves session
+    local any = false
+    local list = 'The following buffers are unsaved:\n\n'
+    for _, buffer in ipairs(textadept.buffers) do
+      if buffer.dirty then
+        list = list..(buffer.filename or 'Untitled')..'\n'
+        any = true
+      end
+    end
+    if any then
+      list = list..'\nQuit without saving?'
+      if os.execute('zenity --question --title Alert '..
+        '--text "'..list..'"') ~= 0 then
+        return false
+      end
+    end
+    textadept.io.save_session()
+    return true
+  end)
+
+
+---
+-- Shows completions for the current command_entry text.
+-- Opens a new buffer (if one hasn't already been opened) for printing possible
+-- completions.
+-- @param command The command to complete.
+function show_completions(command)
+  local textadept = textadept
+  local match_buffer, goto
+  if buffer.shows_completions then
+    match_buffer = buffer
+  else
+    for index, buffer in ipairs(textadept.buffers) do
+      if buffer.shows_completions then
+        match_buffer = index
+        goto = buffer.doc_pointer ~= textadept.focused_doc_pointer
+      elseif buffer.doc_pointer == textadept.focused_doc_pointer then
+        textadept.prev_buffer = index
+      end
+    end
+    if not match_buffer then
+      match_buffer = textadept.new_buffer()
+      match_buffer.shows_completions = true
+    else
+      if goto then view:goto(match_buffer) end
+      match_buffer = textadept.buffers[match_buffer]
+    end
+  end
+  match_buffer:clear_all()
+
+  local substring = command:match('[%w_%.]+$')
+  local path, prefix = (substring or ''):match('^([%w_.]-)%.?([%w_]*)$')
+  local ret, tbl = pcall(loadstring('return ('..path..')'))
+  if not ret then tbl = getfenv(0) end
+  if type(tbl) ~= 'table' then return end
+  for k in pairs(tbl) do
+    if type(k) == 'string' and k:match('^'..prefix) then
+      match_buffer:add_text(k..'\n')
+    end
+  end
+  match_buffer:set_save_point()
+end
+
+---
+-- Hides the completion buffer if it is currently focused and restores the
+-- previous focused buffer (if possible).
+function hide_completions()
+  local textadept = textadept
+  if buffer.shows_completions then
+    buffer:close()
+    if textadept.prev_buffer then view:goto_buffer(textadept.prev_buffer) end
+  end
+end
+
+---
+-- Default error handler.
+-- Opens a new buffer (if one hasn't already been opened) for printing errors.
+-- @param ... Error strings.
+function error(...)
+  local function handle_error(...)
+    local textadept = textadept
+    local error_message = table.concat( {...} , '\n' )
+    local error_buffer
+    for index, buffer in ipairs(textadept.buffers) do
+      if buffer.shows_errors then
+        error_buffer = buffer
+        if buffer.doc_pointer ~= textadept.focused_doc_pointer then
+          view:goto_buffer(index)
+        end
+        break
+      end
+    end
+    if not error_buffer then
+      error_buffer = textadept.new_buffer()
+      error_buffer.shows_errors = true
+    end
+    error_buffer:append_text(error_message..'\n')
+    error_buffer:set_save_point()
+  end
+  pcall( handle_error, unpack{...} ) -- prevent endless loops if this errors
+end
