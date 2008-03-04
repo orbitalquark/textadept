6d985dc6785d5c73a43072a8b1e0e68eb8c0a4ee
mitchell <70453897+orbitalquark@users.noreply.github.com>
2008-03-03 21:58:51 -0500

    Updated copyright notice to include the year 2008.

--- core/ext/pm/find_browser.lua
+++ core/ext/pm/find_browser.lua
@@ -1,4 +1,4 @@
--- Copyright 2007 Mitchell mitchell<att>caladbolg.net. See LICENSE.
+-- Copyright 2007-2008 Mitchell mitchell<att>caladbolg.net. See LICENSE.
 
 ---
 -- Find in files browser for the Textadept project manager.
