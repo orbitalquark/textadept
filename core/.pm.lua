fec8608be67a594ce072b4165fdf0a15f0572ea5
mitchell <70453897+667e-11@users.noreply.github.com>
2008-06-17 20:59:53 -0400

    Updated copyright year information for LuaDoc files.

--- core/.pm.lua
+++ core/.pm.lua
@@ -1,4 +1,4 @@
--- Copyright 2007 Mitchell mitchell<att>caladbolg.net. See LICENSE.
+-- Copyright 2007-2008 Mitchell mitchell<att>caladbolg.net. See LICENSE.
 -- This is a DUMMY FILE used for making LuaDoc for built-in functions in the
 -- global textadept.pm table.
 
