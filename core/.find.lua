48dbd27c0a79cd73ab662fc75d2c4dd0c95b5702
mitchell <70453897+667e-11@users.noreply.github.com>
2008-07-09 00:44:54 -0400

    Added LuaDoc from core/ext/find.lua to core/.find.lua.

--- core/.find.lua
+++ core/.find.lua
@@ -23,3 +23,44 @@ find = { find_entry_text = nil, replace_entry_text = nil }
 
 --- Displays and focuses the find/replace dialog.
 function focus() end
+
+---
+-- [Local table] Text escape sequences with their associated characters.
+-- @class table
+-- @name escapes
+local escapes = {}
+
+---
+-- Finds and selects text in the current buffer.
+-- This is used by the find dialog. It is recommended to use the buffer:find()
+-- function for scripting.
+-- @param text The text to find.
+-- @param flags Search flags. This is a number mask of 3 flags: match case (2),
+--   whole word (4), and Lua pattern (8) joined with binary AND.
+-- @param next Flag indicating whether or not the search direction is forward.
+-- @param nowrap Flag indicating whether or not the search won't wrap.
+-- @param wrapped Utility flag indicating whether or not the search has wrapped
+--   for displaying useful statusbar information. This flag is used and set
+--   internally, and should not be set otherwise.
+function find.find(text, flags, next, nowrap, wrapped) end
+
+---
+-- Replaces found text.
+-- This function is used by the find dialog. It is not recommended to call it
+-- via scripts.
+-- textadept.find.find is called first, to select any found text. The selected
+-- text is then replaced by the specified replacement text.
+-- @param rtext The text to replace found text with. It can contain both Lua
+--   capture items (%n where 1 <= n <= 9) for Lua pattern searches and %()
+--   sequences for embedding Lua code for any search.
+function find.replace(rtext) end
+
+---
+-- Replaces all found text.
+-- This function is used by the find dialog. It is not recommended to call it
+-- via scripts.
+-- @param ftext The text to find.
+-- @param rtext The text to replace found text with.
+-- @param flags The number mask identical to the one in 'find'.
+-- @see find.find
+function find.replace_all(ftext, rtext, flags) end
