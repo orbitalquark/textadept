519feb108e4baa5363f0df64a8e5eeedafb7dd7a
mitchell <70453897+orbitalquark@users.noreply.github.com>
2008-08-09 15:03:19 -0400

    Added SciTE theme for Textadept; themes/scite.lua

---
+++ themes/scite.lua
@@ -0,0 +1,27 @@
+-- Copyright 2007-2008 Mitchell mitchell<att>caladbolg.net. See LICENSE.
+-- SciTE editor theme for Textadept.
+
+local buffer, c = buffer, textadept.constants
+
+buffer.margin_width_n[0] = 4 * buffer:text_width(c.STYLE_LINENUMBER, "9")
+
+buffer.caret_period = 500
+
+-- fold margin
+buffer.margin_type_n[2] = c.SC_MARGIN_SYMBOL
+buffer.margin_width_n[2] = 16
+buffer.margin_mask_n[2] = c.SC_MASK_FOLDERS
+buffer.margin_sensitive_n[2] = true
+
+-- fold margin markers
+buffer:marker_define(c.SC_MARKNUM_FOLDEROPEN, c.SC_MARK_MINUS)
+buffer:marker_set_fore(c.SC_MARKNUM_FOLDEROPEN, 16777215)
+buffer:marker_set_back(c.SC_MARKNUM_FOLDEROPEN, 0)
+buffer:marker_define(c.SC_MARKNUM_FOLDER, c.SC_MARK_PLUS)
+buffer:marker_set_fore(c.SC_MARKNUM_FOLDER, 16777215)
+buffer:marker_set_back(c.SC_MARKNUM_FOLDER, 0)
+buffer:marker_define(c.SC_MARKNUM_FOLDERSUB, c.SC_MARK_EMPTY)
+buffer:marker_define(c.SC_MARKNUM_FOLDERTAIL, c.SC_MARK_EMPTY)
+buffer:marker_define(c.SC_MARKNUM_FOLDEREND, c.SC_MARK_EMPTY)
+buffer:marker_define(c.SC_MARKNUM_FOLDEROPENMID, c.SC_MARK_EMPTY)
+buffer:marker_define(c.SC_MARKNUM_FOLDERMIDTAIL, c.SC_MARK_EMPTY)
