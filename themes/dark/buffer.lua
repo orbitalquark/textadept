24e35c7708efcebd0180ffc235c891d07af298dc
mitchell <70453897+orbitalquark@users.noreply.github.com>
2008-12-22 13:08:27 -0500

    Light theme is the default; moved old dark default into its own theme.

---
+++ themes/dark/buffer.lua
@@ -0,0 +1,20 @@
+-- Copyright 2007-2008 Mitchell mitchell<att>caladbolg.net. See LICENSE.
+-- Dark editor theme for Textadept.
+
+local textadept, buffer = textadept, buffer
+
+-- folding
+buffer.property['fold'] = '1'
+buffer.property['fold.by.indentation'] = '1'
+
+-- tabs and indentation
+buffer.tab_width = 2
+buffer.use_tabs = false
+buffer.indent = 2
+buffer.tab_indents = true
+buffer.back_space_un_indents = true
+buffer.indentation_guides = 1
+
+-- various
+buffer.eol_mode = textadept.constants.SC_EOL_LF
+buffer.auto_c_choose_single = true
