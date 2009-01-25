b11a7d994072bc6ed723b92a9d2646fb91397d78
mitchell <70453897+orbitalquark@users.noreply.github.com>
2009-01-25 16:25:11 -0500

    Updated core/ext/pm/buffer_browser.lua to update the PM cursor appropriately.

--- core/ext/pm/buffer_browser.lua
+++ core/ext/pm/buffer_browser.lua
@@ -65,10 +65,21 @@ function perform_menu_action(menu_item, menu_id, selected_item)
 end
 
 local function update_view()
-  if matches(textadept.pm.entry_text) then textadept.pm.activate() end
+  if matches(textadept.pm.entry_text) then
+    textadept.pm.activate()
+    for idx, buf in ipairs(textadept.buffers) do
+      if buf == buffer then
+        textadept.pm.cursor = idx - 1
+        break
+      end
+    end
+  end
 end
 textadept.events.add_handler('file_opened', update_view)
 textadept.events.add_handler('buffer_new', update_view)
 textadept.events.add_handler('buffer_deleted', update_view)
 textadept.events.add_handler('save_point_reached', update_view)
 textadept.events.add_handler('save_point_left', update_view)
+textadept.events.add_handler('buffer_switch', update_view)
+textadept.events.add_handler('view_switch', update_view)
+
