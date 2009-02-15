571d487056863bf0c2f3abc98d61c8bf345d53b0
mitchell <70453897+orbitalquark@users.noreply.github.com>
2009-02-15 14:46:45 -0500

    Minor refactoring, more encapsulation, and code cleanup.

--- src/textadept.h
+++ src/textadept.h
@@ -34,8 +34,6 @@ extern GtkWidget *window, *focused_editor, *command_entry, *pm_container,
                  *pm_entry, *pm_view, *findbox, *find_entry, *replace_entry,
                  *fnext_button, *fprev_button, *r_button, *ra_button,
                  *match_case_opt, *whole_word_opt, *lua_opt, *in_files_opt;
-extern GtkTreeStore *pm_store;
-extern lua_State *lua;
 #if !(WIN32 || MAC)
 static const char *textadept_home = "/usr/share/textadept/";
 #else
@@ -57,13 +55,8 @@ void split_window(GtkWidget *editor, bool vertical);
 bool unsplit_window(GtkWidget *editor);
 void set_menubar(GtkWidget *menubar);
 void set_statusbar_text(const char *text, bool docbar);
-
-GtkWidget *pm_create_ui();
 void pm_toggle_focus();
-
-GtkWidget *find_create_ui();
 void find_toggle_focus();
-
 void ce_toggle_focus();
 
 // lua_interface.c
@@ -79,24 +72,23 @@ void l_remove_scintilla_buffer(sptr_t doc);
 void l_goto_scintilla_buffer(GtkWidget *editor, int n, bool absolute);
 void l_set_buffer_global(ScintillaObject *sci);
 
-void l_handle_error(lua_State *lua, const char *errmsg);
+void l_handle_error(const char *errmsg);
 bool l_handle_event(const char *e, const char *arg=NULL);
 bool l_handle_keypress(int keyval, bool shift, bool control, bool alt);
 void l_handle_scnnotification(SCNotification *n);
 void l_ta_popup_context_menu(GdkEventButton *event);
 
-void l_pm_view_fill(GtkTreeIter *initial_iter);
-void l_pm_perform_action(GtkTreePath *path);
-void l_pm_popup_context_menu(GtkTreePath *path, GdkEventButton *event,
-                             GCallback callback);
-void l_pm_perform_menu_action(GtkTreePath *path, int menu_id);
+void l_pm_view_fill(GtkTreeStore *store, GtkTreeIter *initial_iter);
+void l_pm_perform_action(GtkTreeStore *store, GtkTreePath *path);
+void l_pm_popup_context_menu(GtkTreeStore *store, GtkTreePath *path,
+                             GdkEventButton *event, GCallback callback);
+void l_pm_perform_menu_action(GtkTreeStore *store, GtkTreePath *path, int id);
 
 void l_find(const char *ftext, bool next);
 void l_find_replace(const char *rtext);
 void l_find_replace_all(const char *ftext, const char *rtext);
 
 void l_ce_command(const char *command);
-bool l_cec_get_completions_for(const char *entry_text);
-void l_cec_populate(GtkListStore *store);
+void l_cec_fill(GtkListStore *store);
 
 #endif
