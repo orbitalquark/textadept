939a72efd44d86e1a4bf5102e63d6d1d1982967c
mitchell <70453897+orbitalquark@users.noreply.github.com>
2010-03-02 23:21:57 -0500

    Removed default fallback themes; core/events.lua

--- core/events.lua
+++ core/events.lua
@@ -270,50 +270,6 @@ add_handler('view_new',
       if ret then return end
       io.stderr:write(errmsg)
     end
-
-    -- Default Theme (Light).
-
-    -- caret
-    buffer.caret_fore = 3355443 -- 0x33 | 0x33 << 8 | 0x33 << 16
-    buffer.caret_line_visible = true
-    buffer.caret_line_back = 14540253 -- 0xDD | 0xDD << 8 | 0xDD << 16
-    buffer:set_x_caret_policy(1, 20) -- CARET_SLOP
-    buffer:set_y_caret_policy(13, 1) -- CARET_SLOP | CARET_STRICT | CARET_EVEN
-
-    -- selection
-    buffer:set_sel_fore(1, 3355443) -- 0x33 | 0x33 << 8 | 0x33 << 16
-    buffer:set_sel_back(1, 10066329) -- 0x99 | 0x99 << 8 | 0x99 << 16
-
-    buffer.margin_width_n[0] = -- line number margin
-      4 + 3 * buffer:text_width(c.STYLE_LINENUMBER, '9')
-
-    buffer.margin_width_n[1] = 0 -- marker margin invisible
-
-    -- fold margin
-    buffer:set_fold_margin_colour(1, 13421772) -- 0xCC | 0xCC << 8 | 0xCC << 16
-    buffer:set_fold_margin_hi_colour(1, 13421772) -- 0xCC | 0xCC << 8 | 0xCC << 16
-    buffer.margin_type_n[2] = c.SC_MARGIN_SYMBOL
-    buffer.margin_width_n[2] = 10
-    buffer.margin_mask_n[2] = c.SC_MASK_FOLDERS
-    buffer.margin_sensitive_n[2] = true
-
-    -- fold margin markers
-    buffer:marker_define(c.SC_MARKNUM_FOLDEROPEN, c.SC_MARK_ARROWDOWN)
-    buffer:marker_set_fore(c.SC_MARKNUM_FOLDEROPEN, 0)
-    buffer:marker_set_back(c.SC_MARKNUM_FOLDEROPEN, 0)
-    buffer:marker_define(c.SC_MARKNUM_FOLDER, c.SC_MARK_ARROW)
-    buffer:marker_set_fore(c.SC_MARKNUM_FOLDER, 0)
-    buffer:marker_set_back(c.SC_MARKNUM_FOLDER, 0)
-    buffer:marker_define(c.SC_MARKNUM_FOLDERSUB, c.SC_MARK_EMPTY)
-    buffer:marker_define(c.SC_MARKNUM_FOLDERTAIL, c.SC_MARK_EMPTY)
-    buffer:marker_define(c.SC_MARKNUM_FOLDEREND, c.SC_MARK_EMPTY)
-    buffer:marker_define(c.SC_MARKNUM_FOLDEROPENMID, c.SC_MARK_EMPTY)
-    buffer:marker_define(c.SC_MARKNUM_FOLDERMIDTAIL, c.SC_MARK_EMPTY)
-
-    -- various
-    buffer.call_tip_use_style = 0
-    buffer:set_fold_flags(16)
-    buffer.mod_event_mask = c.SC_MOD_CHANGEFOLD
   end)
 
 add_handler('buffer_new',
@@ -334,23 +290,6 @@ add_handler('buffer_new',
         if ret then return end
         io.stderr:write(errmsg)
       end
-
-      -- Default theme (Light).
-
-      -- folding
-      buffer.property['fold'] = '1'
-      buffer.property['fold.by.indentation'] = '1'
-
-      -- tabs and indentation
-      buffer.tab_width = 2
-      buffer.use_tabs = false
-      buffer.indent = 2
-      buffer.tab_indents = true
-      buffer.back_space_un_indents = true
-      buffer.indentation_guides = 1
-
-      -- various
-      buffer.auto_c_choose_single = true
     end
     -- normally when an error occurs, a new buffer is created with the error
     -- message, but if an error occurs here, this event would be called again
