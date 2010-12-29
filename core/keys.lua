ac1a1ab03ef259d18053be47a31ff389eb88e45c
mitchell <70453897+667e-11@users.noreply.github.com>
2010-12-29 15:58:47 -0500

    Fix bug for gui.statusbar_text being nil; core/keys.lua

--- core/keys.lua
+++ core/keys.lua
@@ -257,8 +257,8 @@ local function keypress(code, shift, control, alt)
         -- Clear the key sequence, but keep any status messages from the key
         -- command itself.
         keychain = {}
-        if gui.statusbar_text == L('Invalid sequence') or
-           gui.statusbar_text:find('^'..L('Keychain:')) then
+        local text = gui.statusbar_text or ''
+        if text == L('Invalid sequence') or text:find('^'..L('Keychain:')) then
           gui.statusbar_text = ''
         end
       end
