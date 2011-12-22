2a278b7aaae98906024a2b2d9809b0514118b3ec
mitchell <70453897+667e-11@users.noreply.github.com>
2011-12-21 21:18:37 -0500

    Updated for 5.0 alpha.

--- core/init.lua
+++ core/init.lua
@@ -1,6 +1,6 @@
 -- Copyright 2007-2011 Mitchell mitchell<att>caladbolg.net. See LICENSE.
 
-_RELEASE = "Textadept 4.3"
+_RELEASE = "Textadept 5.0 beta"
 
 package.path = _HOME..'/core/?.lua;'..package.path
 os.setlocale('C', 'collate')
