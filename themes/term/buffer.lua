ad85d064bf4e540c5ecf65093f302fb79ee58bdc
mitchell <70453897+667e-11@users.noreply.github.com>
2012-06-02 06:30:41 -0400

    Added terminal theme for ncurses.

---
+++ themes/term/buffer.lua
@@ -0,0 +1,16 @@
+-- Copyright 2007-2012 Mitchell mitchell<att>caladbolg.net. See LICENSE.
+-- Terminal editor theme for Textadept.
+
+local buffer = buffer
+
+-- Folding.
+buffer.property['fold'] = '1'
+buffer.property['fold.by.indentation'] = '1'
+buffer.property['fold.line.comments'] = '0'
+
+-- Tabs and Indentation.
+buffer.tab_width = 2
+buffer.use_tabs = false
+buffer.indent = 2
+buffer.tab_indents = true
+buffer.back_space_un_indents = true
