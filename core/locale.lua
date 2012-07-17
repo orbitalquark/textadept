17c33b24243cccf82298513e5a99888ec5b9976e
mitchell <70453897+667e-11@users.noreply.github.com>
2012-07-17 15:30:37 -0400

    Remove mnemonics from localized ncurses strings; core/locale.lua

--- core/locale.lua
+++ core/locale.lua
@@ -17,7 +17,7 @@ if not f then error('"core/locale.conf" not found.') end
 for line in f:lines() do
   if not line:find('^%s*%%') then
     local id, str = line:match('^(.-)%s*=%s*(.+)$')
-    if id and str then M[id] = str end
+    if id and str then M[id] = not NCURSES and str or str:gsub('_', '') end
   end
 end
 f:close()
