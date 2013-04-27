8045f96321e12bf40999a2f6893a75be0f28ce3a
mitchell <70453897+667e-11@users.noreply.github.com>
2013-04-27 02:04:53 -0400

    Updated for 6.6 beta.

--- core/init.lua
+++ core/init.lua
@@ -1,6 +1,6 @@
 -- Copyright 2007-2013 Mitchell mitchell.att.foicica.com. See LICENSE.
 
-_RELEASE = "Textadept 6.5"
+_RELEASE = "Textadept 6.6 beta"
 
 package.path = _HOME..'/core/?.lua;'..package.path
 
