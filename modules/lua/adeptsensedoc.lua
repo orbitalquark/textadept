5c24f45d314bef8651d736e154399bcf88c97c3e
mitchell <70453897+667e-11@users.noreply.github.com>
2014-01-12 23:49:03 -0500

    Updated copyright information.

--- modules/lua/adeptsensedoc.lua
+++ modules/lua/adeptsensedoc.lua
@@ -1,4 +1,4 @@
--- Copyright 2007-2013 Mitchell mitchell.att.foicica.com. See LICENSE.
+-- Copyright 2007-2014 Mitchell mitchell.att.foicica.com. See LICENSE.
 
 -- Adeptsense doclet for LuaDoc.
 -- This module is used by LuaDoc to create an adeptsense for Lua with a fake
