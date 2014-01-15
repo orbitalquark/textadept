cb0a8dbdfef18f11837ada591af7e99b793b95a6
mitchell <70453897+667e-11@users.noreply.github.com>
2014-01-14 21:56:39 -0500

    Added Swedish translation from Niklas Wallén.

--- core/locale.lua
+++ core/locale.lua
@@ -11,7 +11,7 @@ module('_L')]]
 
 local f = io.open(_USERHOME..'/locale.conf', 'rb')
 if not f then
-  local lang = (os.getenv('LANG') or ''):match('^[^_.@]+')
+  local lang = (os.getenv('LANG') or ''):match('^[^_.@]+') -- TODO: LC_MESSAGES?
   if lang then f = io.open(_HOME..'/core/locales/locale.'..lang..'.conf') end
 end
 if not f then f = io.open(_HOME..'/core/locale.conf', 'rb') end
