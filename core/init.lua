9434383cb208be41f867a735e0eb59c9a7ef77d9
mitchell <70453897+orbitalquark@users.noreply.github.com>
2014-04-10 22:02:41 -0400

    Updated for 7.2 beta 4.

--- core/init.lua
+++ core/init.lua
@@ -1,6 +1,6 @@
 -- Copyright 2007-2014 Mitchell mitchell.att.foicica.com. See LICENSE.
 
-_RELEASE = "Textadept 7.2 beta 3"
+_RELEASE = "Textadept 7.2 beta 4"
 
 package.path = _HOME..'/core/?.lua;'..package.path
 
