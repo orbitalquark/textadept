cc700abb63ef2dec1349277095baf123639723c7
mitchell <70453897+orbitalquark@users.noreply.github.com>
2015-01-20 23:30:22 -0500

    Check for modified files after resuming from suspend; core/file_io.lua

--- core/file_io.lua
+++ core/file_io.lua
@@ -309,6 +309,7 @@ end
 events_connect(events.BUFFER_AFTER_SWITCH, update_modified_file)
 events_connect(events.VIEW_AFTER_SWITCH, update_modified_file)
 events_connect(events.FOCUS, update_modified_file)
+events_connect(events.RESUME, update_modified_file)
 
 -- Prompts the user to reload the current file if it has been externally
 -- modified.
