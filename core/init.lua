e8be43d53e2f8271bdaec7de1f32ee736b6a469f
mitchell <70453897+667e-11@users.noreply.github.com>
2015-04-19 21:35:54 -0400

    Updated for 8.0 beta.

--- core/init.lua
+++ core/init.lua
@@ -1,6 +1,6 @@
 -- Copyright 2007-2015 Mitchell mitchell.att.foicica.com. See LICENSE.
 
-_RELEASE = "Textadept 8.0 alpha"
+_RELEASE = "Textadept 8.0 beta"
 
 package.path = _HOME..'/core/?.lua;'..package.path
 
