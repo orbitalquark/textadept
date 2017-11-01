17065ddce68bdae5ebdcd6282d1b3837f10e4135
mitchell <70453897+667e-11@users.noreply.github.com>
2017-10-31 20:15:56 -0400

    Updated for 9.6.

--- core/init.lua
+++ core/init.lua
@@ -1,6 +1,6 @@
 -- Copyright 2007-2017 Mitchell mitchell.att.foicica.com. See LICENSE.
 
-_RELEASE = "Textadept 9.5"
+_RELEASE = "Textadept 9.6"
 _COPYRIGHT = 'Copyright © 2007-2017 Mitchell. See LICENSE.\n'..
              'http://foicica.com/textadept'
 
