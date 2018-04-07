710e07f040d532201b02bceb38d1eb704ec90eaf
mitchell <70453897+orbitalquark@users.noreply.github.com>
2018-04-07 18:50:17 -0400

    Fixed restoration of vertical scroll for buffers with different line heights. In particular, the Markdown lexer produces different line heights than most other lexers, so set the lexer as soon as possible in order for the correct scroll calculations to be made further down the `events.BUFFER_AFTER_SWITCH` handler line.

--- modules/textadept/file_types.lua
+++ modules/textadept/file_types.lua
@@ -102,7 +102,7 @@ end)
 -- Restores the buffer's lexer, primarily for the side-effect of emitting
 -- `events.LEXER_LOADED`.
 local function restore_lexer() buffer:set_lexer(buffer._lexer) end
-events.connect(events.BUFFER_AFTER_SWITCH, restore_lexer)
+events.connect(events.BUFFER_AFTER_SWITCH, restore_lexer, 1)
 events.connect(events.VIEW_AFTER_SWITCH, restore_lexer)
 events.connect(events.VIEW_NEW, restore_lexer)
 events.connect(events.RESET_AFTER, restore_lexer)
