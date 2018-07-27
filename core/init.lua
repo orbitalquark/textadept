9099326ce8d4192c994e4bfed4a96b193d330e24
mitchell <70453897+orbitalquark@users.noreply.github.com>
2018-07-26 21:18:50 -0400

    Updated for 10.0.

--- core/init.lua
+++ core/init.lua
@@ -1,6 +1,6 @@
 -- Copyright 2007-2018 Mitchell mitchell.att.foicica.com. See LICENSE.
 
-_RELEASE = "Textadept 10.0 beta 2"
+_RELEASE = "Textadept 10.0"
 _COPYRIGHT = 'Copyright © 2007-2018 Mitchell. See LICENSE.\n'..
              'http://foicica.com/textadept'
 
