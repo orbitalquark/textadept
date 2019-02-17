289683eb2044abce8350b8df18580e4d41e072fb
mitchell <70453897+667e-11@users.noreply.github.com>
2019-02-16 22:57:47 -0500

    Updated copyright year.

--- src/textadept.c
+++ src/textadept.c
@@ -1,4 +1,4 @@
-// Copyright 2007-2018 Mitchell mitchell.att.foicica.com. See LICENSE.
+// Copyright 2007-2019 Mitchell mitchell.att.foicica.com. See LICENSE.
 
 #if __linux__
 #define _XOPEN_SOURCE 500 // for readlink from unistd.h
