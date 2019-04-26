6c9c32d9838bb5d79a4a20d6935b4a7c0b64336f
mitchell <70453897+orbitalquark@users.noreply.github.com>
2019-04-26 18:10:01 -0400

    Updated for 10.4.

--- core/init.lua
+++ core/init.lua
@@ -1,6 +1,6 @@
 -- Copyright 2007-2019 Mitchell mitchell.att.foicica.com. See LICENSE.
 
-_RELEASE = "Textadept 10.3"
+_RELEASE = "Textadept 10.4"
 _COPYRIGHT = 'Copyright © 2007-2019 Mitchell. See LICENSE.\n'..
              'http://foicica.com/textadept'
 
