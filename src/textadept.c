3c9e3aa4a1add3ae33d54b8886034b3fcf861720
mitchell <70453897+orbitalquark@users.noreply.github.com>
2020-07-08 10:30:55 -0400

    Reverted accidental commit of experimental changes.

--- src/textadept.c
+++ src/textadept.c
@@ -828,21 +828,6 @@ static int menu(lua_State *L) {
 #endif
 }
 
-/** `ui.update()` Lua function. */
-static int update_ui(lua_State *L) {
-#if GTK
-  while (gtk_events_pending()) gtk_main_iteration();
-#elif CURSES
-  struct timeval timeout = {0, 1e5}; // 0.1s
-  int nfds = os_spawn_pushfds(L);
-  fd_set *fds = (fd_set *)lua_touserdata(L, -1);
-  while (select(nfds, fds, NULL, NULL, &timeout) > 0)
-    if (os_spawn_readfds(L) >= 0) refresh_all();
-  lua_pop(L, 1); // fd_set
-#endif
-  return 0;
-}
-
 /** `ui.__index` Lua metamethod. */
 static int ui_index(lua_State *L) {
   const char *key = lua_tostring(L, 2);
@@ -1605,7 +1590,6 @@ static int init_lua(lua_State *L, int argc, char **argv, int reinit) {
   lua_pushcfunction(L, get_split_table), lua_setfield(L, -2, "get_split_table");
   lua_pushcfunction(L, goto_view), lua_setfield(L, -2, "goto_view");
   lua_pushcfunction(L, menu), lua_setfield(L, -2, "menu");
-  lua_pushcfunction(L, update_ui), lua_setfield(L, -2, "update");
   set_metatable(L, -1, "ta_ui", ui_index, ui_newindex);
   lua_setglobal(L, "ui");
 
