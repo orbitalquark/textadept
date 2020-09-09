bc27dc37f97edba91091d280354787cf939b2d98
mitchell <70453897+orbitalquark@users.noreply.github.com>
2020-09-09 09:39:21 -0400

    Simplified and generalized documentation generation script.

--- scripts/fill_layout.lua
+++ scripts/fill_layout.lua
@@ -5,15 +5,13 @@
 
 -- Filter the file through markdown using TOC generation in order to get header
 -- anchors, but ignore the actual TOC.
-local name = arg[1]
-local f = io.open(name, 'r')
-local markdown = f:read('*a')
-f:close()
-local p = io.popen('markdown -f toc -T ' .. name)
+local p = io.popen('markdown -f toc -T ' .. arg[1])
 local html = p:read('*a'):match('^.-\n</ul>\n(.+)$')
 p:close()
 
 -- Fill in HTML layout (stdin) with markdown output and print the result.
-local title, content = '{{ page.title }}', '{{ content }}'
-io.write(io.stdin:read('*a'):gsub(title, html:match('<h%d.->([^<]+)')):
-  gsub(content, (html:gsub('%%', '%%%%'))))
+local tags = {
+  ['page.title'] = html:match('<h%d.->([^<]+)'),
+  content = html:gsub('%%', '%%%%')
+}
+io.write(io.stdin:read('*a'):gsub('{{ (%S+) }}', tags))
