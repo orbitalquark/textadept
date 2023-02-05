c49f453e04c8f9a0bdc3be10813b793d5a648fdf
mitchell <70453897+orbitalquark@users.noreply.github.com>
2023-02-05 12:54:36 -0500

    Better error handling for syntax highlighting.

--- core/lexer.lua
+++ core/lexer.lua
@@ -46,11 +46,13 @@ local function set_lexer(buffer, name)
   lexer.property['scintillua.lexers'] = _LEXERPATH
   rawset(buffer, 'lexer', lexer.load(name))
   rawset(buffer, 'lexer_language', name)
-  rawset(buffer, 'named_styles', #buffer.lexer._TAGS)
-  if buffer.lexer._CHILDREN then
-    local ws = {} -- style numbers considered to be whitespace styles in multi-language lexers
-    for i = 1, view.STYLE_MAX do ws[i] = buffer:name_of_style(i):find('whitespace') ~= nil end
-    if #ws > 0 then buffer._ws = ws end
+  if rawget(buffer, 'lexer') then
+    rawset(buffer, 'named_styles', #buffer.lexer._TAGS)
+    if buffer.lexer._CHILDREN then
+      local ws = {} -- style numbers considered to be whitespace styles in multi-language lexers
+      for i = 1, view.STYLE_MAX do ws[i] = buffer:name_of_style(i):find('whitespace') ~= nil end
+      if #ws > 0 then buffer._ws = ws end
+    end
   end
 
   -- Update styles, forward folding properties to the lexer, copy lexer-specific properties to
@@ -73,62 +75,69 @@ end
 
 -- Documentation is in core/.buffer.luadoc.
 local function name_of_style(buffer, style_num)
-  return buffer.lexer._TAGS[assert_type(style_num, 'number', 2)] or 'Unknown'
+  local lexer = rawget(buffer, 'lexer')
+  return lexer and lexer._TAGS[assert_type(style_num, 'number', 2)] or 'Unknown'
 end
 
 -- Documentation is in core/.buffer.luadoc.
 local function style_of_name(buffer, name)
-  return buffer.lexer._TAGS[assert_type(name, 'string', 2):gsub('_', '.')] or view.STYLE_DEFAULT
+  local lexer = rawget(buffer, 'lexer')
+  return lexer and lexer._TAGS[assert_type(name, 'string', 2):gsub('_', '.')] or view.STYLE_DEFAULT
 end
 
--- Performs syntax highlighting as needed.
+-- Performs syntax highlighting in buffer *buffer* from *start_pos* to *end_pos*.
 -- Start from the beginning of the current style so the lexer can match the tag.
 -- For multilang lexers, start at whitespace since embedded languages have whitespace.[lang]
 -- styles. This is so the lexer can start matching child languages instead of parent ones
 -- if necessary.
-events.connect(events.STYLE_NEEDED, function(pos, buffer)
-  local s = buffer:position_from_line(buffer:line_from_position(buffer.end_styled))
-  if not rawget(buffer, 'lexer') then
-    buffer:start_styling(s, 0)
-    buffer:set_styling(pos - s, view.STYLE_DEFAULT)
-    return
-  end
+-- @param buffer A buffer.
+-- @param start_pos Position to start syntax highlighting at.
+-- @param end_pos Position to stop syntax highlighting at.
+local function highlight(buffer, start_pos, end_pos)
   local style_at, ws = buffer.style_at, buffer._ws
-  local init_style = s > 1 and style_at[s - 1] or view.STYLE_DEFAULT
-  while s > 1 and style_at[s - 1] == init_style do s = s - 1 end
-  if ws then while s > 1 and not ws[style_at[s]] do s = s - 1 end end
+  local init_style = start_pos > 1 and style_at[start_pos - 1] or view.STYLE_DEFAULT
+  while start_pos > 1 and style_at[start_pos - 1] == init_style do start_pos = start_pos - 1 end
+  if ws then while start_pos > 1 and not ws[style_at[start_pos]] do start_pos = start_pos - 1 end end
 
   -- Setup buffer-specific lexer fields.
   local name_of_style, line_from_position = buffer.name_of_style, buffer.line_from_position
   lexer.style_at = setmetatable({}, {
-    __index = function(_, pos) return name_of_style(buffer, style_at[s + pos - 1]) end
+    __index = function(_, pos) return name_of_style(buffer, style_at[start_pos + pos - 1]) end
   })
   rawset(lexer, 'fold_level', buffer.fold_level) -- override legacy compatibility
-  lexer.line_from_position = function(pos) return line_from_position(buffer, s + pos - 1) end
+  lexer.line_from_position =
+    function(pos) return line_from_position(buffer, start_pos + pos - 1) end
   lexer.line_state, lexer.indent_amount = buffer.line_state, buffer.line_indentation
 
   -- Invoke the lexer and style text from the returned table of tags.
-  buffer:start_styling(s, 0)
-  local ok, styles = pcall(lexer.lex, buffer.lexer, buffer:text_range(s, pos), init_style)
-  if not ok then
-    buffer:set_styling(pos - s, view.STYLE_DEFAULT)
-    events.emit(events.ERROR, styles)
-    return
-  end
-  local set_styling, tags, p = buffer.set_styling, buffer.lexer._TAGS, 1
+  buffer:start_styling(start_pos, 0)
+  local styles = buffer.lexer:lex(buffer:text_range(start_pos, end_pos), init_style)
+  local set_styling, tags, pos = buffer.set_styling, buffer.lexer._TAGS, 1
   for i = 1, #styles, 2 do
     local e = styles[i + 1]
-    set_styling(buffer, e - p, tags[styles[i]])
-    p = e
+    set_styling(buffer, e - pos, tags[styles[i]])
+    pos = e
   end
-  buffer:set_styling(pos - (s + p - 1), view.STYLE_DEFAULT)
+  buffer:set_styling(end_pos - (start_pos + pos - 1), view.STYLE_DEFAULT)
 
   -- Invoke the folder and fold the text from the returned table of fold levels.
-  local line = buffer:line_from_position(s)
-  s = buffer:position_from_line(line)
+  local line = buffer:line_from_position(start_pos)
+  start_pos = buffer:position_from_line(line)
   local level = buffer.fold_level[line] & buffer.FOLDLEVELNUMBERMASK
-  local folds = lexer.fold(buffer.lexer, buffer:text_range(s, pos), line, level)
+  local folds = buffer.lexer:fold(buffer:text_range(start_pos, end_pos), line, level)
   for line, level in pairs(folds) do buffer.fold_level[line] = level end
+end
+
+-- Performs syntax highlighting as needed.
+events.connect(events.STYLE_NEEDED, function(end_pos, buffer)
+  local start_pos = buffer:position_from_line(buffer:line_from_position(buffer.end_styled))
+  local ok, errmsg
+  if rawget(buffer, 'lexer') then ok, errmsg = pcall(highlight, buffer, start_pos, end_pos) end
+  if not ok then
+    buffer:start_styling(start_pos, 0)
+    buffer:set_styling(end_pos - start_pos, view.STYLE_DEFAULT)
+    if errmsg then error(errmsg, 2) end
+  end
 end)
 
 -- Gives new buffers lexer-specific functions and sets a default lexer.
