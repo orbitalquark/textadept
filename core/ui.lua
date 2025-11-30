86513fea7669b6f525cc4c4ad2cc387a825b5aad
Jamie Drinkell <94254558+M0JXD@users.noreply.github.com>
2025-11-30 03:54:04 +0000

    Enable SGR mouse mode for control beyond 223 columns

--- core/ui.lua
+++ core/ui.lua
@@ -338,8 +338,14 @@ end)
 -- Handle mouse events and functionality in the terminal version.
 if CURSES then
 	if not WIN32 then
-		local function enable_mouse() io.stdout:write("\x1b[?1002h"):flush() end
-		local function disable_mouse() io.stdout:write("\x1b[?1002l"):flush() end
+		local function enable_mouse()
+			io.stdout:write("\x1b[?1002h"):flush()
+			io.stdout:write("\x1b[?1006h"):flush()
+		end
+		local function disable_mouse()
+			io.stdout:write("\x1b[?1002l"):flush()
+			io.stdout:write("\x1b[?1006l"):flush()
+		end
 		events.connect(events.INITIALIZED, enable_mouse)
 		events.connect(events.SUSPEND, disable_mouse)
 		events.connect(events.RESUME, enable_mouse)
