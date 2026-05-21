aadf6c5fd3f81abfb54a7c0b662c9fb02d7e01f3
orbitalquark <70453897+orbitalquark@users.noreply.github.com>
2026-05-20 23:47:31 -0400

    Temporarily output use of deprecated OS/platform variables.
    This should help aid migration of user scripts.

--- init.lua
+++ init.lua
@@ -15,7 +15,7 @@ textadept = require('textadept')
 
 -- Temporary backward-compatibility.
 -- LuaFormatter off
-for k,v in pairs{windows='WIN32',macos='OSX',linux='LINUX',bsd='BSD',terminal='CURSES',gtk='GTK',qt='QT'} do if OS==k or UI==k then _G[v]=true end end
+local bools,usages={WIN32='windows',OSX='macos',LINUX='linux',BSD='bsd',CURSES='terminal',GTK='gtk',QT='qt'},{}setmetatable(_G,{__index=function(t, k)if not bools[k]then return nil end local info=debug.getinfo(2,'Sl')local usage=string.format("%s:%s: '%s' is deprecated and should be replaced by an OS or UI comparison\n",info.source:sub(2),info.currentline,k)if usages then usages[#usages+1]=usage else ui.output_silent(usage)end return OS==bools[k] or UI==bools[k]end})events.connect(events.INITIALIZED,function()if #usages>0 then ui.output(table.concat(usages))end usages=nil end)
 -- LuaFormatter on
 
 -- The remainder of this file defines default buffer and view properties and applies them
